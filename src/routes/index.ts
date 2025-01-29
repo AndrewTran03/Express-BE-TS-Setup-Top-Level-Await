9bc07acb814c9fb076b245e3c9870acc47d3025e
Andrew Tran <andrewtranva@gmail.com>
2025-01-29 12:13:44 -0500

    Added another API route to test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { TEST_ROUTER } from "./test.js";
+import { PERSON_ROUTER } from "./person.js";
 
 const router = Router();
 
 router.use(TEST_ROUTER);
+router.use(PERSON_ROUTER);
 
 export { router as APP_ROUTER };
